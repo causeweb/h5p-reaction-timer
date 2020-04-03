505269e82e546f86e376be8074b6e9a7727092f5
ROBERT PATRICK CAREY III <rpc5102@psu.edu>
2020-04-03 16:16:46 -0400

    Pushing changes

--- reactiontimer.js
+++ reactiontimer.js
@@ -9,12 +9,17 @@ H5P.ReactionTimer = (function ($) {
     H5P.EventDispatcher.call(this);
     // Extend defaults with provided options
     this.options = $.extend(true, {}, {
+      instructions: '',
       repetitions: 5,
-      countdownTime: 0
+      countdownTime: 0,
+      waitTimeGroup: {
+        waitTimeMin: 1,
+        waitTimeMax: 5
+      }
     }, options);
     // Keep provided id.
     this.id = id;
-    
+
     this.stimuli = null;
     this.timeStart = 0;
     this.timeStop = 0;
@@ -42,55 +47,107 @@ H5P.ReactionTimer = (function ($) {
     // container.  Allows for styling later.
     $container.addClass("h5p-reactiontimer");
 
-    $container.append(`
-    <div class="h5p-instructions">
-      <strong>Instructions</strong>
-      <ul>
-        <li>Click the <strong>Start</strong> button to begin the trials</li>
-        <li>Wait the prescribed time period</li>
-        <li>Click within the circle when directed</li>
-        <li>Continue until completed</li>
-      </ul>
-    </div>`);
+    if(this.options.instructions) {
+      $container.append(`
+      <div class="h5p-instructions">
+        <strong>Instructions:</strong>
+        ` + this.options.instructions + `
+      </div>`);
+    }
 
-    $container.append(`
-    <div class="stimuli-wrapper">
-      <button id="stimuli" class="stimuli" type="button" disabled>
-        <span class="inner-content" data-content="Click start"></span>
-      </button>
-    </div>`);
-    
-    $container.append(`
-    <div class="h5p-controls">
-      <button id="advancementBtn" class="h5p-joubelui-button h5p-button reactiontimer-startbutton">Start</button>
-      <button id="resetBtn" class="h5p-joubelui-button h5p-button reactiontimer-retrybutton">Reset</button>
-    </div>
-    `);
+    let stimuliOuter = $('<div/>', {
+      'class': 'h5p-stimuli-outer'
+    });
 
-    //$container.find('.h5p-controls').append(advancementBtn);
+    let stimuli = $('<button/>', {
+      'id': 'h5p-stimuli',
+      'class': 'stimuli'
+    })
+    .attr('disabled', true)
+    .click(function() {
+      self.trigger('stimuliInteraction')
+    });
     
+    let stimuliInner = $('<span/>', {
+      'id': 'h5p-stimuli-inner',
+      'class': 'inner-content'
+    });
+
+    let results = $('<div/>', {
+      'class': 'h5p-results'
+    });
+
+    $('.h5p-results').hide();
+
+    $(stimuliOuter).append(stimuli);
+    $(stimuli).append(stimuliInner);
+    $container.append(stimuliOuter);
+    $container.append(results);
+
+    let controls = $('<div/>', { 'class': 'h5p-controls'});
+
+    let advancementBtn = H5P.JoubelUI.createSimpleRoundedButton('Start');
+        advancementBtn.addClass('rt-advancementbutton');
+        advancementBtn.attr('id', 'advancementBtn');
+
+    let resetBtn = H5P.JoubelUI.createSimpleRoundedButton('Reset');
+        resetBtn.addClass('rt-resetbutton');
+        resetBtn.attr('id', 'resetBtn');
+
+    let resultsBtn = H5P.JoubelUI.createSimpleRoundedButton('Show Results');
+      resultsBtn.addClass('rt-resultsbutton');
+      resultsBtn.attr('id', 'resultsBtn');
+      resultsBtn.hide();
+
+    $(controls).append(resultsBtn, advancementBtn, resetBtn);
+
+    $container.append(controls);
+
     // Show number of summary of trial
     $container.append(`
     <dl class="h5p-status">
-      <dt class="repetitions-text">Average:</dt>
-      <dd><span id="average">` + self.getAverageTime() + `</span>
+      <dt class="repetitions-text">Attempts:</dt>
+      <dd>
+        <span id="attempts"></span>
+        <span id="tries">` + self.attempts + `</span> of <span id="repetitions">` + self.options.repetitions + `</span>
       </dd>
-      <dt class="repetitions-text">Tries:</dt>
-      <dd><span id="attempts">` + self.attempts + `</span> of <span id="repetitions">` + self.options.repetitions + `</span>
+      <dt class="repetitions-text">Average (mean):</dt>
+      <dd><span id="average">` + self.getAverageTime('sec') + `</span>
       </dd>
     </dl>
     `);
 
-    this.stimuli = document.getElementById('stimuli');
-
-    this.stimuli.addEventListener('click', function (event) {
-      if (!self.hasCompleted()) {
-        self.disableStimuli();
-        self.handleResponse(event);
-      }
+    $(advancementBtn).click(function() {
+      self.trigger('advancement');
     });
 
-    document.getElementById('advancementBtn').addEventListener('click', () => {
+    $(resetBtn).click(function() {
+      self.trigger('reset');
+    });
+
+    $(resultsBtn).click(function() {
+      self.trigger('showResults');
+    });
+
+    this.on('stimuliInteraction', function (event) {
+       if (!self.hasCompleted()) {
+        /* If waiting; responded too quickly */
+        if(this.waiting){
+          /* Ignore response and clear */
+          clearTimeout(self.timeout);
+          setTimeout(() => {
+            this.responded = false;
+            this.stimuli.classList.remove('invalid');
+            this.setStimuliText('Retry');
+            this.disableAdvancement(false);
+          }, 1500); /* Give time to digest message */
+        }
+        self.disableStimuli();
+        self.handleResponse(event);
+       }
+    });
+
+    this.on('advancement', function () {
       this.disableAdvancement(true);
       if (!self.completed) {
         self.startTrial();
@@ -99,10 +156,31 @@ H5P.ReactionTimer = (function ($) {
       }
     });
 
-    document.getElementById('resetBtn').addEventListener('click', () => {
+    this.on('reset', function () {
       self.reset();
     });
+
+    this.on('showResults', function () {
+      self.showResults();
+    });
+
+    this.stimuli = document.getElementById('h5p-stimuli');
   };
+
+  C.prototype.triggerXAPI = function (verb = 'interacted') {
+    var xAPIEvent = this.createXAPIEventTemplate(verb);
+
+    if(verb == 'attempted') {
+
+    } else if (verb == 'completed') {
+
+    } else /* interacted */ {
+
+    }
+
+    this.trigger(xAPIEvent);
+    console.log(xAPIEvent);
+  }
 
   C.prototype.startTrial = function () {
     let waitTime = this.options.countdownTime ? this.options.countdownTime : 0;
@@ -116,12 +194,6 @@ H5P.ReactionTimer = (function ($) {
           this.waiting = false;
           this.setStimuliText('Click!');
           this.startTimer();
-        } else {
-          /* Ignore previous response and clear */
-          this.responded = false;
-          this.stimuli.classList.remove('invalid');
-          this.setStimuliText('Retry');
-          this.disableAdvancement(false);
         }
       }, timer);
     });
@@ -149,6 +221,27 @@ H5P.ReactionTimer = (function ($) {
     let total = this.reactionTimes.reduce((a, b) => a + b, 0);
     this.average = Math.round(total / this.reactionTimes.length);
     return average;
+  }
+
+  C.prototype.computeResults = function () {
+    let percentile = this.getPercentile();
+    let digit = percentile.toString().slice(-1);
+    let ending = '';
+
+    if (digit == "1") {
+      ending = "st";
+    } else if (digit == "2") {
+      ending = "nd";
+    } else if (digit == "3") {
+      ending = "rd";
+    } else {
+      ending = "th";
+    }
+
+    $('.h5p-results').html(`<p>Your average (mean) time of <b>${this.getAverageTime('sec')}</b> ranks in the <b>${percentile}${ending}</b> percentile of all recorded times.</p>
+    <div class="sample-data"></div>`);
+
+    $('#resultsBtn').hide();
   }
 
   C.prototype.countdown = function (wait = 0) {
@@ -179,7 +272,22 @@ H5P.ReactionTimer = (function ($) {
     this.started = value;
   }
 
-  C.prototype.randWaitTime = function (min = 1, max = 3) {
+  C.prototype.sampleTimes = function() {
+    const sample = [172.4377,188.7274,205.997,209.9946,232.6494,241.7846,247.5073,256.1078,265.738,277.3069,278.4246,288.502,293.1073,301.6239,318.6322,323.222,324.9026,325.8464,332.3318,335.8619,337.1097,339.9816,345.9363,354.39,357.2129,358.3283,362.5386,365.7027,380.1032,387.1377,387.8648,390.8152,392.5195,392.8928,399.3687,405.6453,409.949,417.5202,421.5327,423.1982,428.0762,432.8053,439.3381,447.744,454.1692,456.8289,458.6833,460.5304,461.7891,463.7745,465.5371,466.4259,468.4489,473.2009,480.5006,488.859,495.1843,499.6697,502.9291,506.768,511.955,513.0341,516.7706,525.4836,533.5727,539.5461,546.5797,549.0832,568.7169,605.6833,615.7043,620.1642,625.9605,626.8282,628.5691,634.4895,639.5857,642.6185,645.2881,646.882,655.3925,673.8686,680.6684,692.2268,705.5426,707.1181,711.379,721.0491,742.7233,760.8832,804.5089,818.6854,842.8914,875.8373,891.2556,937.4281,943.1703,975.8158,1012.7955,1082.0036,1194.6269];
+
+    return sample;
+  }
+
+  C.prototype.getPercentile = function(average = this.average) {
+    let sample = this.sampleTimes();
+    let percentile = sample.indexOf(sample.find(
+      element => element > average
+    ));
+
+    return percentile;
+  }
+
+  C.prototype.randWaitTime = function (min = this.options.waitTimeGroup.waitTimeMin, max = this.options.waitTimeGroup.waitTimeMax) {
     this.waiting = true;
     var min_ms = min * 1000;
     var max_ms = max * 1000;
@@ -191,38 +299,49 @@ H5P.ReactionTimer = (function ($) {
    * If time is less than 60 seconds display as seconds
    * If greater than 60 seconds display N/A
    */
-  C.prototype.formatReactionTime = function (reactionTime = 0) {
+  C.prototype.formatReactionTime = function (reactionTime = 0, format = 'ms', raw = false) {
     let output = 'N/A';
-    if(reactionTime > 0 && reactionTime < 10000) {
-      output = reactionTime + ' ms';
-    } else if(reactionTime >= 10000 && reactionTime < 60000) {
+    if((reactionTime > 0 && reactionTime < 10000) && format == 'ms') {
+      output = raw ? reactionTime : reactionTime + ' ms';
+    } else if((reactionTime >= 10000 && reactionTime < 60000) || (reactionTime && format == 'sec')) {
       /* * *
        * Convert to seconds and
-       * round to 1 decimal place using
+       * round to 3 decimal places using
        * scaling trick: https://stackoverflow.com/a/11832950
        */
       let sec = reactionTime / 1000;
-          sec = Math.round((sec + Number.EPSILON) * 10) / 10;
-      output = sec + ' sec';
+          sec = Math.round((sec + Number.EPSILON) * 1000) / 1000;
+      output = raw ? sec : sec + ' sec';
     }
     return output;
   }
 
-  C.prototype.getAverageTime = function (raw = false) {
-    if (raw) {
+  C.prototype.getAverageTime = function (format = 'ms') {
+    if (format == 'raw') {
       return this.average;
+    } else if (format == 'sec') {
+      return this.formatReactionTime(this.average, format);
     } else {
-      return this.formatReactionTime(this.average);
+      return this.formatReactionTime(this.average, format);
     }
   }
 
-  C.prototype.updateAverage = function (avg) {
-    document.getElementById('average').innerHTML = this.getAverageTime();
+  C.prototype.updateAverage = function () {
+    this.computeAvg();
+    document.getElementById('average').innerHTML = this.getAverageTime('sec');
+  }
+
+  C.prototype.listAttempts = function () {
+    let list = [];
+    this.reactionTimes.forEach(value => {
+      list.push(this.formatReactionTime(value, 'sec', true));
+    });
+    document.getElementById('attempts').innerHTML = '{ ' + list.join(', ') + ' } sec';
   }
 
   C.prototype.updateAttempts = function () {
     this.attempts++;
-    document.getElementById('attempts').innerHTML = this.attempts;
+    document.getElementById('tries').innerHTML = this.attempts;
     this.hasCompleted();
   }
 
@@ -231,13 +350,24 @@ H5P.ReactionTimer = (function ($) {
     return this.completed;
   }
 
+  C.prototype.showResults = function (value = true) {
+    if(value) {
+      this.computeResults();
+      $('.h5p-results').show();
+      $('.h5p-stimuli-outer').hide();
+    } else {
+      $('.h5p-results').hide();
+      $('.h5p-stimuli-outer').show();
+    }
+  }
+
   C.prototype.setStimuliText = function (text) {
     this.stimuli.querySelector('.inner-content').setAttribute('data-content', text);
   }
 
   C.prototype.disableAdvancement = function (value = true) {
     let advancementBtn = document.getElementById('advancementBtn');
-        advancementBtn.disabled = value;
+        advancementBtn.setAttribute('disabled', value);
   }
 
   C.prototype.disableStimuli = function (value = true) {
@@ -249,21 +379,27 @@ H5P.ReactionTimer = (function ($) {
     this.responded = true;
     /* Check if input was too soon */
     if(this.waiting) {
-      this.setStimuliText('Too Soon!');
+      this.triggerXAPI('attempted');
+      this.setStimuliText('Too Soon');
       this.stimuli.classList.add('invalid');
     } else {
       this.storeTime();
-      let avg = this.computeAvg();
       this.stimuli.classList.remove('active');
-      this.updateAverage(avg);
+      this.updateAverage();
       this.updateAttempts();
+      this.listAttempts();
 
+      /* Check if completed, if not, proceed */
       if (this.hasCompleted()) {
-        this.setStimuliText('Complete ✓');
+        this.triggerXAPI('completed');
+        this.setStimuliText('Complete');
         this.stimuli.classList.add('valid');
         this.disableAdvancement(true);
+        $('#advancementBtn').hide();
+        $('#resultsBtn').show();
       } else {
-        this.setStimuliText(this.formatReactionTime(this.reactionTimes[0]));
+        this.triggerXAPI('interacted');
+        this.setStimuliText(this.formatReactionTime(this.reactionTimes[0], 'sec'));
         this.disableAdvancement(false);
       }
       this.responded = false;
@@ -287,14 +423,20 @@ H5P.ReactionTimer = (function ($) {
     /* Clear styling */
     this.disableAdvancement(false);
     document.getElementById('advancementBtn').innerHTML = 'Start';
-    document.getElementById('average').innerHTML = this.getAverageTime();
-    document.getElementById('attempts').innerHTML = this.attempts;
+    document.getElementById('average').innerHTML = this.getAverageTime('sec');
+    document.getElementById('tries').innerHTML = this.attempts;
+    document.getElementById('attempts').innerHTML = '';
     this.stimuli.classList.remove(
       'active',
       'valid',
       'invalid'
     );
-    this.setStimuliText('Click start');
+    this.setStimuliText('');
+
+    /* Hide results */
+    this.showResults(false);
+    $('#resultsBtn').hide();
+    $('#advancementBtn').show();
   }
 
   return C;
